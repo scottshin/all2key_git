bc74805aecb6f02462521c97aa1a61717b473775
scott <devscott@gmail.com>
2026-03-23 08:48:50 +0900

    update

--- All2KeyIME/BuildDll.cmd
+++
@@ -1,7 +0,0 @@
-
-cd /d "f:\All2KeyIME"
-
-"c:\Program Files\Microsoft Visual Studio\18\Insiders\MSBuild\Current\Bin\MSBuild.exe" ./All2KeyIME.sln
-
-
-copy f:\\All2KeyIME\\x64\debug\All2keyIME.dll    c:\\Users\\devscott\\Desktop\\runner\
